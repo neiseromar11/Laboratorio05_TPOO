056836294f2f572ff320ddc085027c437b90b7d9
Neiser omar <49031435+neiseromar11@users.noreply.github.com>
2024-09-16 01:04:09 -0500

    Actualizacion

--- src/laboratorio04/Convertidor.java
+++ src/laboratorio04/Convertidor.java
@@ -4,14 +4,18 @@
  */
 package laboratorio04;
 
+import javax.swing.JTextField;
+
 
 public class Convertidor extends javax.swing.JFrame {
 
-   
+   Logica log;
     public Convertidor() {
         initComponents();
+        log =new Logica(txtValor);
     }
-
+    
+    
   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -26,6 +30,7 @@ public class Convertidor extends javax.swing.JFrame {
         btnCelsius = new javax.swing.JButton();
         btnSoles = new javax.swing.JButton();
         btnPies = new javax.swing.JButton();
+        btnLimpiar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -38,6 +43,11 @@ public class Convertidor extends javax.swing.JFrame {
         radInversa.setText("Inversa");
 
         btnKilos.setText("de Kg a Lb");
+        btnKilos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKilosActionPerformed(evt);
+            }
+        });
 
         btnCelsius.setText("de C° a F°");
         btnCelsius.addActionListener(new java.awt.event.ActionListener() {
@@ -47,8 +57,25 @@ public class Convertidor extends javax.swing.JFrame {
         });
 
         btnSoles.setText("de sol a dolar");
+        btnSoles.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSolesActionPerformed(evt);
+            }
+        });
 
         btnPies.setText("de Pies a Metros");
+        btnPies.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPiesActionPerformed(evt);
+            }
+        });
+
+        btnLimpiar.setText("Limpiar");
+        btnLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimpiarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -56,60 +83,89 @@ public class Convertidor extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(txtValor, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel1))
-                    .addComponent(radInversa)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(lblResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnPies, javax.swing.GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
-                    .addComponent(btnKilos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnSoles, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnCelsius, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
+                                .addComponent(txtValor, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel1)
+                                .addGap(32, 32, 32)
+                                .addComponent(jLabel2)
+                                .addGap(18, 18, 18)
+                                .addComponent(lblResultado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnKilos, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnSoles, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnCelsius, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(16, 16, 16))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnPies, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnLimpiar, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(radInversa)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(38, 38, 38)
-                        .addComponent(btnKilos)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(btnSoles)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(54, 54, 54)
-                                .addComponent(radInversa))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(btnCelsius)
-                                .addGap(12, 12, 12)
-                                .addComponent(btnPies))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(45, 45, 45)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(txtValor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1))
-                        .addGap(28, 28, 28)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(lblResultado))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(45, 45, 45)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtValor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(lblResultado))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnKilos)
+                    .addComponent(btnSoles)
+                    .addComponent(btnCelsius))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnPies)
+                    .addComponent(btnLimpiar)
+                    .addComponent(radInversa))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnCelsiusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCelsiusActionPerformed
+   
+  
     
+    private void btnCelsiusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCelsiusActionPerformed
+
+         if(radInversa.isSelected()){ lblResultado.setText(log.fahrenheitACelsius() +"");
+         }else{lblResultado.setText(log.celsiusAFahrenheit() + "" ); }
     }//GEN-LAST:event_btnCelsiusActionPerformed
+
+    private void btnKilosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKilosActionPerformed
+
+         if(radInversa.isSelected()){ lblResultado.setText(log.librasAKilos() +"");
+         }else{  lblResultado.setText(log.kilosALibras() + "" ); }      
+    }//GEN-LAST:event_btnKilosActionPerformed
+
+    private void btnSolesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSolesActionPerformed
+      
+         if(radInversa.isSelected()){lblResultado.setText(log.dolaresASoles() +"");
+         }else{ lblResultado.setText(log.solesADolares() + "" );}
+    }//GEN-LAST:event_btnSolesActionPerformed
+
+    private void btnPiesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPiesActionPerformed
+       
+         if(radInversa.isSelected()){lblResultado.setText(log.MetrosAPies() +"");
+         }else{lblResultado.setText(log.piesAMetros() + "" );}
+    }//GEN-LAST:event_btnPiesActionPerformed
+
+    private void btnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarActionPerformed
+      txtValor.setText("");
+      lblResultado.setText("");
+    }//GEN-LAST:event_btnLimpiarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -126,6 +182,7 @@ public class Convertidor extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCelsius;
     private javax.swing.JButton btnKilos;
+    private javax.swing.JButton btnLimpiar;
     private javax.swing.JButton btnPies;
     private javax.swing.JButton btnSoles;
     private javax.swing.JLabel jLabel1;
--- src/laboratorio04/Logica.java
+++ src/laboratorio04/Logica.java
@@ -4,10 +4,69 @@
  */
 package laboratorio04;
 
+import javax.swing.JTextField;
+
 /**
  *
  * @author NEISER
  */
 public class Logica {
+    private double valor;
+   private JTextField valorLog;
+    
+    public Logica(JTextField valorLog){
+        this.valorLog=valorLog;
+    }
+   private void convertir(){
+       this.valor=getNumDesTextField(valorLog);
+   }
+    
+    public double celsiusAFahrenheit (){
+        convertir();
+     return (valor * 9/5) + 32;
+    }
+    public double fahrenheitACelsius(){
+        convertir();
+    return (valor - 32) * 5/9;
+    
+    }
+    public double kilosALibras(){
+        convertir();
+    return valor * 2.205;
+    
+    }
+    public double librasAKilos(){
+        convertir();
+    return valor / 2.205;
+    
+    }
+    public double piesAMetros(){
+        convertir();
+    return valor * 0.3048;
+    
+    }
+    public double MetrosAPies(){
+        convertir();
+    return valor / 0.3048;
+    }
+    public double solesADolares(){
+        convertir();
+    return valor * 3.77;
+    }
+    public double dolaresASoles(){
+        convertir();
+    return valor / 3.77;  
+    }
+      private double convertirNum(String text){
+         try{
+             return Double.parseDouble(text);
+         }catch(Exception e){
+             return 0;
+         }
+    }
+    private double getNumDesTextField(JTextField text){
+    String val=text.getText();
+    return convertirNum(val);
+    }
     
 }
